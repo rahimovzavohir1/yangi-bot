2f135744edfbff0092e5369725c9e63cf7d24559
zilola <rahimovzavohir1@gmail.com>
2025-11-10 17:35:57 +0500

    function qoshildi

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const TOKEN = "8438960736:AAEk-pjAckWfED44I1U4HjgOxEJTisKuBeI"
 
 const bot = new TelegramBot (TOKEN, {polling: true});
 
-
+bot.on("message", async function (msg) {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const firstname = msg.chat.first_name;
+  if (text == "/start");
+});
